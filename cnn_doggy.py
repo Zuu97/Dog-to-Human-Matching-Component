57bfbc6f585d4670ed5eac406df79615ee086e8b
Alagiyawanna A.M.A.I.S <41842488+Zuu97@users.noreply.github.com>
2020-11-05 11:59:09 +0530

    Update cnn_doggy.py

--- cnn_doggy.py
+++ cnn_doggy.py
@@ -7,134 +7,95 @@ logging.getLogger('tensorflow').disabled = True
 import base64
 import numpy as np
 from tensorflow.keras.models import model_from_json, Sequential, Model, load_model
-from tensorflow.keras.layers import Activation, Dense, Input, BatchNormalization, LeakyReLU, Reshape, Conv2DTranspose
+from tensorflow.keras.layers import Activation, Dense, Input, BatchNormalization
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras import backend as K
 from matplotlib import pyplot as plt
-from sklearn.neighbors import NearestNeighbors
-from util import load_image_data
+from util import load_image_data, get_test_image
 from variables import *
+np.random.seed(seed)
 
 physical_devices = tf.config.experimental.list_physical_devices('GPU')
 tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 class DoggyCNN(object):
     def __init__(self):
-        if not (os.path.exists(cnn_architecture)  and os.path.exists(cnn_weights)):
-            # train_generator, validation_generator = load_image_data()
-            # self.train_generator = train_generator
-            # self.validation_generator = validation_generator
-            # self.train_step = self.train_generator.samples // batch_size_cnn
-            # self.validation_step = self.validation_generator.samples // batch_size_cnn
+        if not os.path.exists(cnn_weights):
+            train_generator, validation_generator = load_image_data()
+            self.train_generator = train_generator
+            self.validation_generator = validation_generator
+            self.train_step = self.train_generator.samples // batch_size_cnn
+            self.validation_step = self.validation_generator.samples // valid_size_cnn
 
-            self.train_generator, classes = load_image_data()
-            self.train_step = len(classes) // batch_size_cnn
-            self.classes = classes
+    def cnn_encoder_model(self):
+        functional_model = tf.keras.applications.MobileNetV2(
+                                                    weights="imagenet",
+                                                             )
+        functional_model.trainable = False
+        inputs = functional_model.input
 
-    def cnn_autoencoder(self): #MobileNet is not build through sequential API, so we need to convert it to sequential
-        mobilenet_functional = tf.keras.applications.MobileNet()
-        inputs = Input(shape=input_shape)
-        x = inputs
-        for layer in mobilenet_functional.layers[1:]:# remove the softmax in original model. because we have only 3 classes
-            layer.trainable = False
-            x = layer(x)
-
+        x = functional_model.layers[-2].output
         x = Dense(dense_1_cnn, activation='relu')(x)
+        x = Dense(dense_1_cnn, activation='relu')(x)
+        x = BatchNormalization()(x)
         x = Dense(dense_2_cnn, activation='relu')(x)
-        x = BatchNormalization()(x)
-        # outputs = Dense(len(set(self.classes)), activation='relu')(x)
-        
-        x = Dense(dense_3_cnn, use_bias=False)(x)
-        x = BatchNormalization()(x)
-        x = LeakyReLU()(x)
-        x = Reshape(upconv1_dim)(x) # (7, 7, 256)
-
-        x = Conv2DTranspose(fs1, kernal_size, strides=stride2, padding='same', use_bias=False)(x)
-        x = BatchNormalization()(x) # (14, 14, 256)
-        x = LeakyReLU()(x)
-
-        x = Conv2DTranspose(fs2, kernal_size, strides=stride2, padding='same', use_bias=False)(x)
-        x = BatchNormalization()(x) # (28, 28, 128)
-        x = LeakyReLU()(x)
-
-        x = Conv2DTranspose(fs3, kernal_size, strides=stride2, padding='same', use_bias=False)(x)
-        x = BatchNormalization()(x) # (56, 56, 64)
-        x = LeakyReLU()(x)
-
-        x = Conv2DTranspose(fs3, kernal_size, strides=stride2, padding='same', use_bias=False)(x)
-        x = BatchNormalization()(x) # (112, 112, 64)
-        x = LeakyReLU()(x)
-
-        outputs = Conv2DTranspose(n_channels, kernal_size, strides=stride2, padding='same', use_bias=False, activation='tanh')(x)
-                                    # (224, 224, 3)
-
-        model = Model(
-                    inputs=inputs,
-                    outputs=outputs
-                    )
-                    
-        # model.summary()
-        self.model = model
+        x = Dense(dense_2_cnn, activation='relu')(x)
+        x = Dense(dense_3_cnn, activation='relu')(x)
+        x = Dense(dense_3_cnn, activation='relu')(x)
+        outputs = Dense(num_classes, activation='softmax')(x)
+        self.model = Model(
+                            inputs=inputs,
+                            outputs=outputs,
+                            name='CNN_Model'
+                            )
+        self.model.summary()
 
     def train(self):
-        # self.model.compile(
-        #                   optimizer='Adam',
-        #                   loss='categorical_crossentropy',
-        #                   metrics=['accuracy']
-        #                   )
-        # self.model.fit_generator(
-        #                   self.train_generator,
-        #                   steps_per_epoch = self.train_step,
-        #                   validation_data = self.validation_generator,
-        #                   validation_steps = self.validation_step,
-        #                   epochs=epochs_cnn,
-        #                   verbose=verbose
-        #                 )
-
         self.model.compile(
                           optimizer='Adam',
-                          loss='mse'
+                          loss='categorical_crossentropy',
+                          metrics=['accuracy']
                           )
-        self.model.fit(
-                    self.train_generator,
-                    steps_per_epoch = self.train_step,
-                    epochs=epochs_cnn,
-                    verbose=verbose
+        self.model.fit_generator(
+                          self.train_generator,
+                          steps_per_epoch= self.train_step,
+                          validation_data= self.validation_generator,
+                          validation_steps = self.validation_step,
+                          epochs=epochs_cnn,
+                          verbose=verbose
                         )
 
-
     def save_model(self):
-        model_json = self.model.to_json()
-        with open(cnn_architecture, "w") as json_file:
-            json_file.write(model_json)
-        self.model.save_weights(cnn_weights)
+        self.model.save(cnn_weights)
+        print(" CNN Model Saved")
 
     def load_model(self):
         K.clear_session() #clearing the keras session before load model
-        json_file = open(cnn_architecture, 'r')
-        loaded_model_json = json_file.read()
-        json_file.close()
+        self.model = load_model(cnn_weights)
+        print(" CNN Model Loaded")
 
-        self.model = model_from_json(loaded_model_json)
-        self.model.load_weights(cnn_weights)
+    def predictions(self, idx):
+        img, label = get_test_image(idx)
+        img = np.expand_dims(img, axis=0)
+        Pimg = self.model.predict(img)
+        Pimg = Pimg.reshape(*input_shape) 
+        img = img.reshape(*input_shape) 
 
-        # self.model.compile(
-        #                    loss='categorical_crossentropy', 
-        #                    optimizer='Adam', 
-        #                    metrics=['accuracy']
-        #                    )
+        fig=plt.figure(figsize=(8, 6))
+        fig.add_subplot(1, 2, 1)
+        plt.title('Input Image')
+        plt.imshow(img)
 
-        self.model.compile(
-                          optimizer='Adam',
-                          loss='mse'
-                          )
+        fig.add_subplot(1, 2, 2)
+        plt.title('Predicted Image')
+        plt.imshow(Pimg)
+
+        plt.show()
 
     def run(self):
         if os.path.exists(cnn_weights):
-            print("Mobile Net TF Model Loading !")
             self.load_model()
         else:
-            print("Mobile Net TF Model Training !")
-            self.cnn_autoencoder()
+            self.cnn_encoder_model()
             self.train()
             self.save_model()
