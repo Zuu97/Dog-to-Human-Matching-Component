dcf55e9de077fb581386a467a27f9f08ce8abaaf
Alagiyawanna A.M.A.I.S <41842488+Zuu97@users.noreply.github.com>
2020-11-08 21:35:12 +0530

    Update cnn_doggy.py

--- cnn_doggy.py
+++ cnn_doggy.py
@@ -4,28 +4,65 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import tensorflow as tf
 import logging
 logging.getLogger('tensorflow').disabled = True
-import base64
 import numpy as np
 from tensorflow.keras.models import model_from_json, Sequential, Model, load_model
 from tensorflow.keras.layers import Activation, Dense, Input, BatchNormalization
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras import backend as K
 from matplotlib import pyplot as plt
-from util import load_image_data, get_test_image
 from variables import *
 np.random.seed(seed)
 
 physical_devices = tf.config.experimental.list_physical_devices('GPU')
 tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
-class DoggyCNN(object):
+def get_class_names():
+    return os.listdir(train_dir)
+
+def preprocessing_function(img):
+    img = tf.keras.applications.mobilenet_v2.preprocess_input(img)
+    return img
+
+def load_image_data():
+    train_datagen = tf.keras.preprocessing.image.ImageDataGenerator(
+                                    rotation_range = 20,
+                                    shear_range = 0.2,
+                                    zoom_range = 0.15,
+                                    width_shift_range= 0.20,
+                                    height_shift_range= 0.20,
+                                    horizontal_flip = True,
+                                    validation_split= 0.15,
+                                    preprocessing_function=preprocessing_function
+                                    )
+
+    train_generator = train_datagen.flow_from_directory(
+                                    train_dir,
+                                    target_size = target_size,
+                                    color_mode = color_mode,
+                                    batch_size = 12,
+                                    classes = get_class_names(),
+                                    subset = 'training',
+                                    shuffle = True)
+
+    validation_generator = train_datagen.flow_from_directory(
+                                    train_dir,
+                                    target_size = target_size,
+                                    color_mode = color_mode,
+                                    batch_size = 6,
+                                    classes = get_class_names(),
+                                    subset = 'validation',
+                                    shuffle = True)
+
+    return train_generator, validation_generator
+
+class ConvolutionalNNmodel(object):
     def __init__(self):
         if not os.path.exists(cnn_weights):
             train_generator, validation_generator = load_image_data()
             self.train_generator = train_generator
             self.validation_generator = validation_generator
-            self.train_step = self.train_generator.samples // batch_size_cnn
-            self.validation_step = self.validation_generator.samples // valid_size_cnn
+            self.train_step = self.train_generator.samples // 12
+            self.validation_step = self.validation_generator.samples // 6
 
     def cnn_encoder_model(self):
         functional_model = tf.keras.applications.MobileNetV2(
@@ -35,14 +72,14 @@ class DoggyCNN(object):
         inputs = functional_model.input
 
         x = functional_model.layers[-2].output
-        x = Dense(dense_1_cnn, activation='relu')(x)
-        x = Dense(dense_1_cnn, activation='relu')(x)
+        x = Dense(512, activation='relu')(x)
+        x = Dense(512, activation='relu')(x)
         x = BatchNormalization()(x)
-        x = Dense(dense_2_cnn, activation='relu')(x)
-        x = Dense(dense_2_cnn, activation='relu')(x)
-        x = Dense(dense_3_cnn, activation='relu')(x)
-        x = Dense(dense_3_cnn, activation='relu')(x)
-        outputs = Dense(num_classes, activation='softmax')(x)
+        x = Dense(256, activation='relu')(x)
+        x = Dense(256, activation='relu')(x)
+        x = Dense(64, activation='relu')(x)
+        x = Dense(64, activation='relu')(x)
+        outputs = Dense(5, activation='softmax')(x)
         self.model = Model(
                             inputs=inputs,
                             outputs=outputs,
@@ -61,8 +98,7 @@ class DoggyCNN(object):
                           steps_per_epoch= self.train_step,
                           validation_data= self.validation_generator,
                           validation_steps = self.validation_step,
-                          epochs=epochs_cnn,
-                          verbose=verbose
+                          epochs=3
                         )
 
     def save_model(self):
@@ -73,24 +109,6 @@ class DoggyCNN(object):
         K.clear_session() #clearing the keras session before load model
         self.model = load_model(cnn_weights)
         print(" CNN Model Loaded")
-
-    def predictions(self, idx):
-        img, label = get_test_image(idx)
-        img = np.expand_dims(img, axis=0)
-        Pimg = self.model.predict(img)
-        Pimg = Pimg.reshape(*input_shape) 
-        img = img.reshape(*input_shape) 
-
-        fig=plt.figure(figsize=(8, 6))
-        fig.add_subplot(1, 2, 1)
-        plt.title('Input Image')
-        plt.imshow(img)
-
-        fig.add_subplot(1, 2, 2)
-        plt.title('Predicted Image')
-        plt.imshow(Pimg)
-
-        plt.show()
 
     def run(self):
         if os.path.exists(cnn_weights):
